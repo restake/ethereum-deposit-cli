7825b5f55b32c029951ead730f4835873b3e0aba
Markkus Millend <mm@liangroup.io>
2023-11-01 18:27:54 +0100

    Add additional input checks for validators command

--- cli/subcommands/create_validators/command.ts
+++ cli/subcommands/create_validators/command.ts
@@ -1,7 +1,7 @@
 import { isAddress } from "ethers";
 import { Command, ValidationError } from "cliffy/command/mod.ts";
 
-import { getOverviewTable, promptConfirm, promptMnemonic, promptPassword } from "./mod.ts";
+import { getOverviewTable, promptConfirm, promptMnemonic, promptPassword, ALLOWED_NETWORKS } from "./mod.ts";
 import { ALLOWED_LANGUAGES } from "../mnemonic/mod.ts";
 
 import { generate } from "../../../keygen/mod.ts";
@@ -11,7 +11,7 @@ export const command = new Command()
     .option("--storage-path <storagePath:string>", "Path used for storing mnemonics and deposit data", { default: "./storage" })
     .option("--existing-mnemonic [existingMnemonic:boolean]", "Use an exisiting mnemonic phrase", { default: false })
     .option("--language <language:string>", `Language to use for the mnemonic (${ALLOWED_LANGUAGES.join(", ")})`, { default: "en" })
-    .option("--network <network:string>", "Ethereum network to use", { default: "mainnet" })
+    .option("--network <network:string>", `Ethereum network to use ${ALLOWED_NETWORKS.join(", ")}`, { default: "mainnet" })
     .option("--start-index <startIndex:number>", "Starting index for the validator keys", { default: 0 })
     .arguments("<numValidators:number> <withdrawalAddress:string>")
     .action(async (options, numValidators, withdrawalAddress) => {
@@ -22,7 +22,10 @@ export const command = new Command()
             throw new ValidationError("Invalid withdrawal address");
         }
         if (ALLOWED_LANGUAGES.indexOf(options.language) === -1) {
-            throw new ValidationError("Invalid language");
+            throw new ValidationError(`Invalid language. Language must be one of ${ALLOWED_LANGUAGES.join(", ")}`);
+        }
+        if (ALLOWED_NETWORKS.indexOf(options.network) === -1) {
+            throw new ValidationError(`Invalid network. Network must be one of ${ALLOWED_NETWORKS.join(", ")}`);
         }
 
         const mnemonic = await promptMnemonic(options.existingMnemonic, options.language);
--- cli/subcommands/create_validators/mod.ts
+++ cli/subcommands/create_validators/mod.ts
@@ -1,3 +1,4 @@
 export * from "./prompts.ts";
 export * from "./tables.ts";
+export * from "./settings.ts";
 export { command as validatorsCommand } from "./command.ts";
