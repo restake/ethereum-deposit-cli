40bb8f78b25bd053ecbb5b949ad02b2336df156f
Markkus Millend <mm@liangroup.io>
2023-11-14 15:38:25 +0100

    Add additional tests for keystore

--- keygen/keystore.test.ts
+++ keygen/keystore.test.ts
@@ -1,4 +1,4 @@
-import { assert } from "$std/assert/mod.ts";
+import { assert, assertEquals, assertRejects } from "$std/assert/mod.ts";
 
 import { createKeystores, saveSigningKeystores, verifySigningKeystores } from "./mod.ts";
 import { verifyPassword } from "../keystore/mod.ts";
@@ -23,5 +23,44 @@ Deno.test("Keystore", async (t) => {
         const savedKeystores = await saveSigningKeystores(keystores, storagePath);
 
         assert(await verifySigningKeystores(storagePath, savedKeystores, keystorePassword));
+    });
+
+    await t.step("Should throw an error when unable to save signing keystores", async () => {
+        const keystorePassword = "testpassword123";
+        const storagePath = "/invalid/path/to/storage";
+
+        const keystores = await createKeystores(credentialsFixture, keystorePassword);
+
+        assertRejects(
+            () => saveSigningKeystores(keystores, storagePath),
+            Error,
+            "Unable to save signing keystores",
+        );
+    });
+
+    await t.step("Should throw an error when unable to read a signing keystore", async () => {
+        const keystorePassword = "testpassword123";
+        const storagePath = "./storage";
+
+        const keystores = await createKeystores(credentialsFixture, keystorePassword);
+        const savedKeystores = await saveSigningKeystores(keystores, storagePath);
+
+        await Deno.remove(`${storagePath}/${savedKeystores[0].fileName}`);
+
+        assertRejects(
+            () => verifySigningKeystores(storagePath, savedKeystores, keystorePassword),
+            Error,
+            `Unable to read signing keystore: ${savedKeystores[0].fileName}`,
+        );
+    });
+
+    await t.step("Should return false when a signing keystore password is invalid", async () => {
+        const keystorePassword = "testpassword123";
+        const storagePath = "./storage";
+
+        const keystores = await createKeystores(credentialsFixture, keystorePassword);
+        const savedKeystores = await saveSigningKeystores(keystores, storagePath);
+
+        assertEquals(await verifySigningKeystores(storagePath, savedKeystores, "invalidpassword"), false);
     });
 });
